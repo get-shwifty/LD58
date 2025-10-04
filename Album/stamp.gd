9e2f3fc6d1425886aca57e8b051d260713ee7f8b
Erilhir <jacob.marie.andrea@gmail.com>
2025-10-04 23:53:45 +0200

    Factoring du code

--- Album/stamp.gd
+++ Album/stamp.gd
@@ -12,6 +12,7 @@ func _ready():
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		emit_signal("stamp_clicked", self)
+		get_viewport().set_input_as_handled()
 
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
 		emit_signal("stamp_clicked", self)
---
+++ global.gd
@@ -0,0 +1,32 @@
+extends Node
+
+var ALBUM_DICT = {
+	0: {
+		"Sprite": preload("res://Assets/stamps/lumiere.png"),
+		"Description": "Lumière - Expédition 33" 
+	},
+	1: {
+		"Sprite": preload("res://Assets/stamps/nevaries.png"), 
+		"Description": "Nevaries - Blue Prince" 
+	},
+	2: {
+		"Sprite": preload("res://Assets/stamps/pharloom.png"), 
+		"Description": "Pharloom - Silksong" 
+	},
+	3: {
+		"Sprite": preload("res://Assets/stamps/rapture.png"), 
+		"Description": "Rapture - Bioshock" 
+	},
+	4: {
+		"Sprite": preload("res://Assets/stamps/irithil.png"), 
+		"Description": "Irithil - DarkSouls III"
+	},
+	5: {
+		"Sprite": preload("res://Assets/stamps/leyndell.png"),
+		"Description": "Leyndell - Elden Ring"
+	},
+	6: {
+		"Sprite": preload("res://Assets/stamps/leyndell.png"),
+		"Description": "Leyndell - Elden Ring"
+	}
+}
